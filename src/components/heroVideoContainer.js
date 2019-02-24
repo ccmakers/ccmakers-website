a8a8d776d9d9b721b08ad47d710e211bcca186c5
Rodrigo Passos <rodrigo@hellodative.com>
2019-02-24 14:27:29 -0500

    add hero video to homepage

---
+++ src/components/heroVideoContainer.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import { css } from '@emotion/core'
+
+const style = css`
+  position: relative;
+  width: 100vw;
+  height: 56.25vw;
+  overflow: hidden;
+
+  @media (min-width: 768px) {
+    height: 40vw;
+  }
+
+  @media (min-width: 1200px) {
+    height: 30vw;
+  }
+
+  video {
+    position: absolute;
+    top: 0;
+    width: 100vw;
+    height: 56.25vw;
+    z-index: 1;
+
+    @media (min-width: 768px) {
+      top: -10vw;
+    }
+  }
+
+  .video-overlay {
+    position: absolute;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    z-index: 2;
+
+    h1 {
+      font-size: 4vw;
+      font-weight: bold;
+      color: #1862b5;
+      background-color: rgba(223, 240, 255, .8);
+      border: .5vw solid #2780E3;
+      padding: 2vw 4vw;
+      line-height: 1;
+    }
+  }
+`
+
+const HeroVideoContainer = ({ title }) => {
+  return (
+    <div css={style}>
+      <video
+        loop
+        autoPlay
+        poster="/videos/3d-print-loop.jpg"
+      >
+          <source src="/videos/3d-print-loop.m4v" type="video/mp4" />
+          <source src="/videos/3d-print-loop.ogg" type="video/ogg" />
+          <source src="/videos/3d-print-loop.webm" type="video/webm" />
+      </video>
+      <div className="video-overlay">
+        { title && (<h1>{title}</h1>)}
+      </div>
+    </div>
+  )
+}
+
+export default HeroVideoContainer
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,13 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+import HeroVideoContainer from "../components/heroVideoContainer"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`cape cod`, `makers`, `3d printing`]} />
-    <p>homepage</p>
+    <HeroVideoContainer title="Making The Future" />
   </Layout>
 )
 
