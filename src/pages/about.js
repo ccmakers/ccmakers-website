6d13c5772dd402a01979b2b3f2de9914304cbc73
Rodrigo Passos <rodrigo@hellodative.com>
2019-03-12 09:43:03 -0400

    complete about page content

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import IntroPageHero from "../components/introPageHero";
-import Container from "../components/container";
 import Img from 'gatsby-image'
+import { Link } from "gatsby"
+import Container from "../components/container";
+import IntroPageHero from "../components/introPageHero";
 import RichText from "../components/richText";
+import Callout from "../components/callout";
 
 const AboutPage = (props) => (
   <Layout>
@@ -17,11 +19,18 @@ const AboutPage = (props) => (
       <Img fluid={props.data.imageOne.childImageSharp.fluid} />
       <RichText html={`
         <p>
-          Cape Cod Makers is engaged in promoting the Maker and DIY culture on Cape Cod. We do this through meetups, connecting local makers, hosting classes, group activities, producing the Cape Cod Mini Maker Faire and promoting shared resources and maker spaces. There is an enormous amount of artistic and technical talent on Cape Cod in a wide variety of areas. Also, there are many people who like to learn new things and improve themselves. We are excited about the possibilities of bringing all these people together on and connecting ideas to grow the maker movement on Cape Cod.
+          Cape Cod Makers is engaged in promoting the Maker and DIY culture on Cape Cod. We do this through meetups, connecting local makers, hosting classes, group activities, promoting shared resources and maker spaces. There is an enormous amount of artistic and technical talent on Cape Cod in a wide variety of areas. Also, there are many people who like to learn new things and improve themselves. We are excited about the possibilities of bringing all these people together on and connecting ideas to grow the maker movement on Cape Cod.
         </p>
       `}>
       </RichText>
     </Container>
+
+    <Callout
+      id="join-the movement"
+      title="Ready to join the movement?"
+      text="If you want to know more about the Cape Cod Makers, please visit us during one of our monthly open house events. We will give you a tour, answer your questions, and introduce you to some of our members."
+      link={<Link to="/events">View Events</Link>}
+    />
 
   </Layout>
 )
