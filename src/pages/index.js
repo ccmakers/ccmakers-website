52973ea09fce4e30b24d03dbbc9e02e1ccfa6868
Rodrigo Passos <rodrigo@hellodative.com>
2019-05-04 19:33:28 -0400

    add newsletter sign up

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import React from "react"
 import Layout from "../components/layout"
 import HeroVideoContainer from "../components/heroVideoContainer"
 import SEO from "../components/seo"
+import NewsletterCTA from "../components/NewsletterCTA";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`cape cod`, `makers`, `3d printing`]} />
     <HeroVideoContainer title="Making The Future" />
+    <NewsletterCTA />
   </Layout>
 )
 
