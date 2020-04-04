8af784edce0b4c3804d7867c9a63d6dea7991d4f
Jesse Craig <jesse.craig@globalfoundries.com>
2020-04-04 13:10:06 -0400

    Fixed typo in thankyou page

--- src/pages/thankyou.js
+++ src/pages/thankyou.js
@@ -53,7 +53,7 @@ const ThankYouPage = (props) => (
     <SEO title="Thank You" keywords={[`thank you`, `cape cod`, `makers`]} />
     <IntroPageHero
       title="Thank you for your support!"
-      introText="Please stop by the MakerSpace during one of our events to see all the great projects your contributing to."
+      introText="Please stop by the MakerSpace during one of our events to see all the great projects you're contributing to."
     />
   </Layout>
 )
