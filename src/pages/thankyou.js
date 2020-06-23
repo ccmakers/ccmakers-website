e4354f20c53b356c70a823301eff3f67d62a805f
Rodrigo Passos <rodrigo@hellodative.com>
2020-06-23 16:33:59 -0400

    clean up thankyou page

--- src/pages/thankyou.js
+++ src/pages/thankyou.js
@@ -2,53 +2,8 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import IntroPageHero from "../components/introPageHero";
-import { css } from '@emotion/core'
 
-const style = css`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  margin-bottom: 3rem;
-
-  .map {
-    flex: 1;
-
-    img {
-      width: 100%;
-    }
-  }
-
-  dl {
-    dt {
-      font-weight: bold;
-    }
-
-    dd {
-      margin: 0;
-
-      & + dt {
-        margin-top: 1rem;
-      }
-    }
-  }
-
-  @media (min-width: 576px) {
-    flex-direction: row;
-
-    .map {
-      max-width: 50%;
-      padding-left: 1rem;
-    }
-
-    .contact-content {
-      background-color: #d8edf6;
-      flex: 1;
-      max-width: 50%;
-      padding-left: 1rem;
-    }
-  }
-`
-const ThankYouPage = (props) => (
+const ThankYouPage = () => (
   <Layout>
     <SEO title="Thank You" keywords={[`thank you`, `cape cod`, `makers`]} />
     <IntroPageHero
